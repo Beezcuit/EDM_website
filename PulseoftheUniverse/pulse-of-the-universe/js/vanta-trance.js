6debb5a240a9942dd1a3f7965e6e8bceb213268c
Beezcuit <ayberkkozkan@hotmail.com>
2025-05-21 14:22:40 +0300

    Add files via upload

---
+++ PulseoftheUniverse/pulse-of-the-universe/js/vanta-trance.js
@@ -0,0 +1,19 @@
+// Trance lazer arka planı
+document.addEventListener("DOMContentLoaded", function () {
+    VANTA.NET({
+      el: "#vanta-trance",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      color: 0x00ffea,            // neon cam göbeği
+      backgroundColor: 0x000000,  // tam siyah
+      points: 14.0,
+      maxDistance: 20.0,
+      spacing: 16.0
+    });
+  });
+  
